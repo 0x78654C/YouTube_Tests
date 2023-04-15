e298a05dc161d9913a82a0ab3b50a03dc155770f
x_Coding <xcoding.dev@gmail.com>
2023-04-15 14:30:53 +0300

    created comboBox tests for selections

---
+++ Guru99/Src/PageObject/Pages/DropDown.cs
@@ -0,0 +1,27 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+using SeleniumExtras.PageObjects;
+
+namespace Guru99.Src.PageObject.Pages
+{
+    public class DropDown
+    {
+        private IWebDriver _driver;
+
+
+        public DropDown(IWebDriver driver, string url)
+        {
+            _driver = driver;
+            PageFactory.InitElements(driver, this);
+            driver.Navigate().GoToUrl(url);
+        }
+
+        public string SelectComboItem(string item) 
+        {
+            var dropBox = new SelectElement(_driver.FindElement(By.Id("awf_field-91977689")));
+            dropBox.SelectByValue(item);
+            var text =  _driver.FindElement(By.XPath($"//option[@value = '{item}']")).Text; 
+            return text;
+        }
+    }
+}
---
+++ Guru99/Test/Script/TestCombo.cs
@@ -0,0 +1,63 @@
+﻿using Guru99.Src.PageObject.Pages;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+using SeleniumExtras.PageObjects;
+
+namespace Guru99.Test.Script
+{
+    public class TestCombo
+    {
+        private IWebDriver? _driver;
+        public DropDown _dropDown;
+        public Global _global;
+
+
+        [SetUp]
+        public void Initialization()
+        {
+            _driver = new ChromeDriver();
+            _driver.Manage().Window.Maximize();
+            _global = new Global();
+            _dropDown = new DropDown(_driver, _global.urlGuru);
+        }
+
+        [Test]
+        [TestCase("sapfico99")]
+        public void ItemSelection1(string item)
+        {
+            var selectedItemText = _dropDown.SelectComboItem(item);
+            Assert.That(selectedItemText,Is.EqualTo( "SAP FICO"));
+        }
+
+        [Test]
+        [TestCase("awlist4227327")]
+        public void ItemSelection2(string item)
+        {
+            var selectedItemText = _dropDown.SelectComboItem(item);
+            Assert.That(selectedItemText, Is.EqualTo("SAP Beginner"));
+        }
+
+        [Test]
+        [TestCase("sap-abap")]
+        public void ItemSelection3(string item)
+        {
+            var selectedItemText = _dropDown.SelectComboItem(item);
+            Assert.That(selectedItemText, Is.EqualTo("SAP ABAP"));
+        }
+
+        [Test]
+        [TestCase("sapbasis")]
+        public void ItemSelection4(string item)
+        {
+            var selectedItemText = _dropDown.SelectComboItem(item);
+            Assert.That(selectedItemText, Is.EqualTo("SAP Basis"));
+        }
+
+        [TearDown]
+        public void CloseConnection()
+        {
+            _driver.Quit();
+        }
+    }
+}
