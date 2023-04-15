6e7b5197e328508e318fd379fc7726068962e493
x_Coding <xcoding.dev@gmail.com>
2023-04-15 15:18:00 +0300

    created email submition tests

---
+++ Guru99/Src/PageObject/Pages/EmailSubmition.cs
@@ -0,0 +1,32 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+using SeleniumExtras.PageObjects;
+
+
+namespace Guru99.Src.PageObject.Pages
+{
+    public class EmailSubmition
+    {
+        private IWebDriver _driver;
+
+        [FindsBy(How = How.XPath, Using = "//input[@placeholder = 'Enter Email']")]
+        private IWebElement _emailTextBox;
+
+
+        [FindsBy(How = How.Id, Using = "philadelphia-field-submit")]
+        private IWebElement _submitMail;
+
+        public EmailSubmition(IWebDriver driver, string url)
+        {
+            _driver = driver;
+            PageFactory.InitElements(driver, this);
+            driver.Navigate().GoToUrl(url);
+        }
+
+        public void SubmitEmail(string email)
+        {
+            _emailTextBox.SendKeys(email);
+            _submitMail.SendKeys("ENTER");
+        }
+    }
+}
---
+++ Guru99/Test/Script/TestEmailSubmition.cs
@@ -0,0 +1,40 @@
+﻿using Guru99.Src.PageObject.Pages;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+using SeleniumExtras.PageObjects;
+
+namespace Guru99.Test.Script
+{
+    public class TestEmailSubmition
+    {
+        private IWebDriver? _driver;
+        private EmailSubmition _submition;
+        public Global _global;
+
+
+        [SetUp]
+        public void Initialization()
+        {
+            _driver = new ChromeDriver();
+            _driver.Manage().Window.Maximize();
+            _global = new Global();
+            _submition = new EmailSubmition(_driver, _global.urlGuru);
+        }
+
+        [Test]
+        [TestCase("new@email.com")]
+        [TestCase("new1@email.com")]
+        [TestCase("new2@email.com")]
+        [TestCase("new3@email.com")]
+        [TestCase("new4@email.com")]
+        [TestCase("new5@email.com")]
+        public void SubmitMails(string email)
+        {
+            _submition.SubmitEmail(email);
+        }
+
+        [TearDown]
+        public void Cleanup() { _driver.Quit(); }
+    }
+}
